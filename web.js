fb51623ccdb06e90aed8b58baa33dca4feec9be2
Mike Kusold <Kusold@git>
2013-07-16 17:25:19 +0000

    specified encoding

--- web.js
+++ web.js
@@ -4,12 +4,16 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(buffer.toString());
+  var options = {
+    encoding: "utf-8"
+  };
+
+  var buffer = fs.readFileSync('index.html', options);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
